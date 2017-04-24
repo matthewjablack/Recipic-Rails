c391d41985cbed0f05c8c6f6ff9268b889a7127c
Matthew Black <mblackmblackmblack@gmail.com>
2017-04-24 14:49:03 -0400

    add search for finding recipes with item (ordered)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
 	has_many :items, :through => :recipeitems
 
 	def self.with_items(*item_ids)
-		joins(:items).where(items: {id: item_ids}).select("#{table_name}.*, COUNT(#{Item.table_name}.id AS match_count").group("#{table_name}.id").order(:match_count)
+		joins(:items).where(items: {id: item_ids}).group("recipes.id").select("recipes.*, COUNT(items.id) AS match_count").order("match_count")
 	end
+
 end
